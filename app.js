87e2f0b8ae1e00e70abf633d1185ad190f49caf0
Aftab Javed <aftabjavedaj10@gmail.com>
2026-02-02 20:28:19 +0500

    Update app.js for Railway deployment

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const db = require("./config/db");
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", require("./routes/auth"));
 
+// Example route
 app.get("/", (req, res) => {
-  res.send("Aftab Cloud ERP Backend Running");
+  res.send("Aftab Autos ERP Backend is running!");
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
